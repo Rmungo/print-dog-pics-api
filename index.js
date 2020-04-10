7c2dbe4dcb891c87151f3650901e1f46754163b0
Rashaan Mungo <rmungo49@gmail.com>
2020-04-10 13:43:17 -0700

    final

---
+++ index.js
@@ -0,0 +1,36 @@
+function getValue(){
+    $('.enterNum').on('click', function(event){
+        event.preventDefault();
+        $('.showPics').empty();
+            numOfPics = $('.typeNum').val();
+        if (numOfPics > 50){
+            alert('Choose a number between 1 and 50');
+        }
+        else {
+            getPics();
+        }
+    })
+}
+
+function getPics(){
+    for (i = 1; i <= numOfPics; i++){
+        fetchPics();
+    }
+}
+
+function fetchPics(){
+    fetch('https://dog.ceo/api/breeds/image/random').then(response => response.json()).then(responseJson=> displayPics(responseJson));
+}
+
+function displayPics(x){
+    $(`.showPics`).append(`<img src="${x.message}">`)
+}
+
+
+function ready(){
+    console.log('we reaaaady');
+    getValue();
+}
+
+
+$(ready());
